2ce7108291777e9f1488e12bdd6c27c5ef8fa6c3
ItsMeSwaron <swaron3214@gmail.com>
2026-02-19 15:58:46 +0600

    Fix: static files serving - add leading slash to STATIC_URL, enable WHITENOISE_USE_FINDERS

--- bookings/settings.py
+++ bookings/settings.py
@@ -216,8 +216,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 STORAGES = {
     'staticfiles': {
         'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
